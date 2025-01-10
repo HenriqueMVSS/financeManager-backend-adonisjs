9f5a4b668d5fcb2c39dddec1d4f7660484e9cb20
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2025-01-09 21:53:07 -0300

    feature: implemented routes.

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,42 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const AuthController = () => import('#controllers/auth/login_controller')
+const ExpensesController = () => import('#controllers/expenses_controller')
+const IncomesController = () => import('#controllers/incomes_controller')
+
+router
+  .group(() => {
+    router.post('/login', [AuthController, 'login'])
+    router
+      .group(() => {
+        router.get('/', [ExpensesController, 'index'])
+        router.get('/:id', [ExpensesController, 'show'])
+        router.post('/', [ExpensesController, 'store'])
+        router.put('/:id', [ExpensesController, 'update'])
+        router.delete('/:id', [ExpensesController, 'destroy'])
+      })
+      .use(
+        middleware.auth({
+          guards: ['api'],
+        })
+      )
+      .prefix('/expenses')
+    router
+      .group(() => {
+        router.get('/', [IncomesController, 'index'])
+        router.get('/:id', [IncomesController, 'show'])
+        router.post('/', [IncomesController, 'store'])
+        router.put('/:id', [IncomesController, 'update'])
+        router.delete('/:id', [IncomesController, 'destroy'])
+      })
+      .use(
+        middleware.auth({
+          guards: ['api'],
+        })
+      )
+      .prefix('/incomes')
+  })
+  .prefix('/api')
